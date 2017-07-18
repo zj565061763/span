bab3f38f1b489ba2610ef457c57c6688fdd84941
zjun <565061763@qq.com>
2017-07-18 16:18:53 +0800

    修改demo

--- app/src/main/java/com/fanwe/www/androidspan/MainActivity.java
+++ app/src/main/java/com/fanwe/www/androidspan/MainActivity.java
@@ -1,58 +1,29 @@
 package com.fanwe.www.androidspan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.style.ImageSpan;
 import android.view.View;
-import android.widget.TextView;
 
-import com.fanwe.library.span.IImageSpanHelper;
-import com.fanwe.library.span.SDImageSpan;
-import com.fanwe.library.span.SDSpannableStringBuilder;
-import com.fanwe.library.span.view.SDSpannableEditText;
+import com.fanwe.library.SDLibrary;
 
 public class MainActivity extends AppCompatActivity
 {
-    private TextView tv;
-    private SDSpannableEditText et_span;
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        SDLibrary.getInstance().init(getApplication());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv = (TextView) findViewById(R.id.tv);
-        et_span = (SDSpannableEditText) findViewById(R.id.et_span);
-
-        testTextView();
-        testEditText();
-    }
-
-    private void testEditText()
-    {
-        tv.setOnClickListener(new View.OnClickListener()
+        findViewById(R.id.btn_textview).setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                et_span.removeSpan();
+                startActivity(new Intent(MainActivity.this, TextViewActivity.class));
             }
         });
-
-        ImageSpan imageSpan = new ImageSpan(this, R.drawable.test);
-        et_span.insertSpan(imageSpan, "key");
     }
 
-    private void testTextView()
-    {
-        SDSpannableStringBuilder sb = new SDSpannableStringBuilder();
-        sb.append("0");
-
-        SDImageSpan imageSpan = new SDImageSpan(this, R.drawable.test);
-        imageSpan.setWidth(100);
-
-        sb.appendSpan(imageSpan, "launcher");
-        tv.setText(sb);
-    }
 
 }
---
+++ app/src/main/java/com/fanwe/www/androidspan/TextViewActivity.java
@@ -0,0 +1,38 @@
+package com.fanwe.www.androidspan;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.fanwe.library.activity.SDBaseActivity;
+import com.fanwe.library.span.IImageSpanHelper;
+import com.fanwe.library.span.SDImageSpan;
+import com.fanwe.library.span.SDSpannableStringBuilder;
+
+/**
+ * Created by Administrator on 2017/7/18.
+ */
+public class TextViewActivity extends SDBaseActivity
+{
+    private TextView tv;
+
+    @Override
+    protected void init(Bundle savedInstanceState)
+    {
+        setContentView(R.layout.act_textview);
+        tv = (TextView) findViewById(R.id.tv);
+
+        SDSpannableStringBuilder sb = new SDSpannableStringBuilder();
+        sb.append("0");
+
+        SDImageSpan span = new SDImageSpan(this, R.drawable.test);
+        span.setVerticalAlignType(IImageSpanHelper.VerticalAlignType.ALIGN_BASELINE); //设置对齐TextView基准线
+        span.setVerticalAlignType(IImageSpanHelper.VerticalAlignType.ALIGN_BOTTOM); //设置对齐TextView底部
+        span.setWidth(100); //设置图片宽度，内部会按比例缩放
+        span.setMarginLeft(10); //设置左边间距
+        span.setMarginRight(10); //设置右边间距
+        span.setMarginBottom(10); //设置底部间距
+
+        sb.appendSpan(span, "launcher");
+        tv.setText(sb);
+    }
+}
