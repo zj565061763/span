8f261aac4f7f8fc9d92732b9f8cfebf6944b3f53
zjun <565061763@qq.com>
2017-07-18 17:27:27 +0800

    增加正则表达式demo

--- app/src/main/java/com/fanwe/www/androidspan/CustomTextView.java
+++ app/src/main/java/com/fanwe/www/androidspan/CustomTextView.java
@@ -31,16 +31,17 @@ public class CustomTextView extends SDSpannableTextView
     @Override
     protected void onProcessSpannableStringBuilder(SDSpannableStringBuilder builder)
     {
+        //正则表达式匹配[***]中括号这种规则的字符串
         List<MatcherInfo> list = SDPatternUtil.findMatcherInfo("\\[([^\\[\\]]+)\\]", builder.toString());
         for (final MatcherInfo info : list)
         {
-            String key = info.getKey();
-            key = key.substring(1, key.length() - 1);
-            int resId = getIdentifierDrawable(key);
+            String key = info.getKey(); //获得匹配的字符串
+            key = key.substring(1, key.length() - 1); //移除中括号，得到文件名
+            int resId = getIdentifierDrawable(key); //根据文件名获得图片资源id
             if (resId != 0)
             {
                 SDImageSpan span = new SDImageSpan(getContext(), resId);
-                builder.setSpan(span, info);
+                builder.setSpan(span, info); //用span，替换匹配到的字符串
             }
         }
     }
