4449904b648729f50a139e5df71ef3a861c52c15
zjun <565061763@qq.com>
2018-09-20 11:56:33 +0800

    refactor

--- app/src/main/java/com/sd/www/androidspan/PatternActivity.java
+++ app/src/main/java/com/sd/www/androidspan/PatternActivity.java
@@ -2,6 +2,12 @@ package com.sd.www.androidspan;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
+import android.widget.TextView;
+
+import com.sd.lib.span.FTextViewPattern;
 
 /**
  * Created by Administrator on 2017/7/18.
@@ -9,14 +15,44 @@ import android.support.v7.app.AppCompatActivity;
 
 public class PatternActivity extends AppCompatActivity
 {
-    private CustomTextView tv;
+    private FTextViewPattern mTextViewPattern;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_pattern);
-        tv = (CustomTextView) findViewById(R.id.tv);
-        tv.setText("fdkfsofosi[face]fdsfsdf[face]");
+        tv = findViewById(R.id.tv);
+        tv.setText("fdkfsofosi[face]fdsfsdf[face]54654655[face]654654");
+
+        getTextViewPattern().setTextView(tv);
+    }
+
+    public FTextViewPattern getTextViewPattern()
+    {
+        if (mTextViewPattern == null)
+        {
+            mTextViewPattern = new FTextViewPattern();
+            mTextViewPattern.addMatchCallback(new FTextViewPattern.MatchCallback()
+            {
+                @Override
+                public String getRegex()
+                {
+                    return "\\[([^\\[\\]]+)\\]";
+                }
+
+                @Override
+                public void onMatch(String regex, String key, int start, int end, SpannableStringBuilder builder)
+                {
+                    final String drawableName = key.substring(1, key.length() - 1);
+                    final int drawableId = getResources().getIdentifier(drawableName, "drawable", getPackageName());
+
+                    final ImageSpan span = new ImageSpan(PatternActivity.this, drawableId);
+                    builder.setSpan(span, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                }
+            });
+        }
+        return mTextViewPattern;
     }
 }
---
+++ lib/src/main/java/com/sd/lib/span/FTextViewPattern.java
@@ -0,0 +1,162 @@
+package com.sd.lib.span;
+
+import android.text.SpannableStringBuilder;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FTextViewPattern
+{
+    private TextView mTextView;
+    private SpannableStringBuilder mBuilder;
+
+    private final List<MatchCallback> mListCallback = new CopyOnWriteArrayList<>();
+
+    /**
+     * 添加正则表达式匹配回调
+     *
+     * @param callback 回调
+     */
+    public void addMatchCallback(MatchCallback callback)
+    {
+        if (callback == null)
+            throw new NullPointerException();
+
+        if (!mListCallback.contains(callback))
+            mListCallback.add(callback);
+    }
+
+    /**
+     * 移除正则表达式匹配回调
+     *
+     * @param regex
+     */
+    public void removeMatchCallback(String regex)
+    {
+        mListCallback.remove(regex);
+    }
+
+    public void setTextView(TextView textView)
+    {
+        final TextView old = mTextView;
+        if (old != textView)
+        {
+            if (old != null)
+            {
+                old.removeOnAttachStateChangeListener(mOnAttachStateChangeListener);
+                addOnPreDrawListener(false, old);
+            }
+
+            mTextView = textView;
+            if (mBuilder != null)
+                mBuilder.clear();
+
+            if (textView != null)
+            {
+                textView.addOnAttachStateChangeListener(mOnAttachStateChangeListener);
+                addOnPreDrawListener(true, textView);
+            }
+        }
+    }
+
+    private void addOnPreDrawListener(boolean add, View view)
+    {
+        final ViewTreeObserver observer = view.getViewTreeObserver();
+        if (observer.isAlive())
+        {
+            observer.removeOnPreDrawListener(mOnPreDrawListener);
+            if (add)
+                observer.addOnPreDrawListener(mOnPreDrawListener);
+        }
+    }
+
+    private final View.OnAttachStateChangeListener mOnAttachStateChangeListener = new View.OnAttachStateChangeListener()
+    {
+        @Override
+        public void onViewAttachedToWindow(View v)
+        {
+            addOnPreDrawListener(true, v);
+        }
+
+        @Override
+        public void onViewDetachedFromWindow(View v)
+        {
+            addOnPreDrawListener(false, v);
+        }
+    };
+
+    private final ViewTreeObserver.OnPreDrawListener mOnPreDrawListener = new ViewTreeObserver.OnPreDrawListener()
+    {
+        @Override
+        public boolean onPreDraw()
+        {
+            processPattern();
+            return true;
+        }
+    };
+
+    private SpannableStringBuilder getBuilder()
+    {
+        if (mBuilder == null)
+            mBuilder = new SpannableStringBuilder();
+        return mBuilder;
+    }
+
+    private void processPattern()
+    {
+        if (mListCallback.isEmpty())
+            return;
+
+        final String textViewContent = mTextView.getText().toString();
+        if (getBuilder().toString().equals(textViewContent))
+            return;
+
+        getBuilder().clear();
+        getBuilder().append(textViewContent);
+
+        for (MatchCallback item : mListCallback)
+        {
+            final String regex = item.getRegex();
+            if (TextUtils.isEmpty(regex))
+                continue;
+
+            final Matcher matcher = Pattern.compile(regex).matcher(textViewContent);
+            if (matcher != null)
+            {
+                while (matcher.find())
+                {
+                    item.onMatch(regex, matcher.group(), matcher.start(), matcher.end(), getBuilder());
+                }
+            }
+        }
+
+        mTextView.setText(getBuilder());
+    }
+
+    public interface MatchCallback
+    {
+        /**
+         * 返回正则表达式
+         *
+         * @return
+         */
+        String getRegex();
+
+        /**
+         * 正则表达式匹配回调
+         *
+         * @param regex   {{@link #getRegex()}}的返回值
+         * @param key     匹配到的关键字
+         * @param start   匹配到的关键字在字符串中的开始位置
+         * @param end     匹配到的关键字在字符串中的结束位置
+         * @param builder
+         */
+        void onMatch(String regex, String key, int start, int end, SpannableStringBuilder builder);
+    }
+}
