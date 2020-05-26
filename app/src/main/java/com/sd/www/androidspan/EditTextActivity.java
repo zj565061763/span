6c594852e7f2c8e914d221edc9cad7f701ed9160
zjun <565061763@qq.com>
2020-05-26 17:51:48 +0800

    androidx

--- app/src/main/java/com/sd/www/androidspan/EditTextActivity.java
+++ app/src/main/java/com/sd/www/androidspan/EditTextActivity.java
@@ -2,7 +2,7 @@ package com.sd.www.androidspan;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.KeyEvent;
--- app/src/main/java/com/sd/www/androidspan/PatternActivity.java
+++ app/src/main/java/com/sd/www/androidspan/PatternActivity.java
@@ -2,7 +2,7 @@ package com.sd.www.androidspan;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
